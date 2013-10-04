5d18005d52f0e5e07e67f2c2e4b562d84992464a
Derek Salama <dereksalama@gmail.com>
2013-10-04 16:57:13 -0400

    forward done

--- ps3/ps3.py
+++ ps3/ps3.py
@@ -1,6 +1,38 @@
 import codecs
 import sys
 
+# scale all probabilities to prevent underflow
+forward_const = 10000000.0
+def forward(emissions, transitions, sequence):
+	unique_states = set(key[0] for key in emissions.keys())
+
+	matrix = {state: [] for state in unique_states}
+	# initialize
+	for state in unique_states:
+		p_s = transitions[("#", state)]
+		p_w = emissions[(state, sequence[0])]
+		matrix[state].append(p_s * p_w * forward_const)
+
+	for (i, c) in enumerate(sequence[1:], start=1):
+		total = 0
+		for s_i in unique_states:
+			p_emission = emissions[(s_i, c)]
+			total_prob = 0
+			for s_j in unique_states:
+				p_transition = transitions[(s_j, s_i)]
+				prev = matrix[s_j][i-1]
+				prob = prev * p_emission * p_transition
+				total_prob += prob
+			matrix[s_i].append(total_prob)
+	for l in matrix.values():
+		for (i, p) in enumerate(l):
+			l[i] = p / forward_const
+
+	total = sum(p for l[:-1] in matrix.values())
+	print "Foward: " + str(total)
+
+
+
 def transitions(file):
 	text = codecs.open(file, 'r', 'utf8')
 	transitions_dict = {}
@@ -8,21 +40,33 @@ def transitions(file):
 		values = line.split()
 		start = values[0]
 		finish = values[1]
-		prob = values[2]
+		prob = float(values[2])
+		transitions_dict[(start, finish)] = prob
 
-		start_dict = transitions_dict.get(start, None)
-		if start_dict is None:
-			start_dict = {}
-			transitions_dict[start] = start_dict
-		start_dict[finish] = prob
 	return transitions_dict
 
+def emissions(file):
+	text = codecs.open(file, 'r', 'utf8')
+	emissions_dict = {}
+	for line in text:
+		values = line.split()
+		state = values[0]
+		emission = values[1]
+		prob = float(values[2])
+		emissions_dict[(state, emission)] = prob
 
+	return emissions_dict
 
 if __name__=='__main__':
 	emissions_file = sys.argv[1]
 	observation_file = sys.argv[2]
 	transitions_file = sys.argv[3]
 
-	print transitions(transitions_file)
+	emissions_dict = emissions(emissions_file)
+	transitions_dict = transitions(transitions_file)
+	sequence = codecs.open(observation_file, 'r', 'utf8').next().split()
+
+	forward(emissions_dict, transitions_dict, sequence)
+
+
 
